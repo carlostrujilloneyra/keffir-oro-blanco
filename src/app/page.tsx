950a940933867c59aa72e778cbfeaba4f4bbc9f1
Carlos Trujillo <augusto21_20@hotmail.com>
2025-02-25 23:40:56 -0500

    fix: changing data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,8 +74,10 @@ export default function HomePage() {
         })
 
         .then((data) => {
-          console.log(data);
-        });
+          return data;
+        })
+
+        .catch((error) => console.log(error));
     };
 
     fetchPosts();
